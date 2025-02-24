6d2c5b5e3dbb9103d149b5101f9550693ccef3d6
Ian Clark <turbana@gmail.com>
2025-02-23 19:19:46 -0800

    added shell script to build emacs from source

---
+++ build-emacs.sh
@@ -0,0 +1,32 @@
+#!/bin/env bash
+
+set -e
+
+CONFIGURE_OPTS="\
+--with-sound=no \
+--with-x-toolkit=gtk3 \
+--with-sqlite3 \
+--with-tree-sitter \
+--with-native-compilation=aot \
+"
+
+if [[ "$#" -ne "1" ]]; then
+    echo "USAGE: $(basename $0) emacs-tag"
+    exit 2
+fi
+
+TAG=$1
+REMOTE=https://ftpmirror.gnu.org/emacs/${TAG}.tar.gz
+TMP=~/tmp
+ROOT=$TMP/$TAG
+
+if [[ ! -d $ROOT ]]; then
+   echo "* downloading $REMOTE into $ROOT"
+   cd $TMP
+   curl -L $REMOTE | tar -xz
+fi
+
+cd $ROOT
+./configure $CONFIGURE_OPTS
+make -j $(nproc --all)
+sudo make install
